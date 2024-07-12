16d3f0d5f325389bc1a5fc7de2a5845feb3e6a7b
David <dbwrush@dbwrush.com>
2024-07-12 16:20:50 -0500

    Add update method for EffectManager

--- src/main/java/net/sudologic/rivals/managers/EffectManager.java
+++ src/main/java/net/sudologic/rivals/managers/EffectManager.java
@@ -2,6 +2,7 @@ package net.sudologic.rivals.managers;
 
 import net.sudologic.rivals.Faction;
 import net.sudologic.rivals.Rivals;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -20,10 +21,6 @@ public class EffectManager {
      */
 
     public void applyEffects(Player player, int intensity) {
-        Faction f = Rivals.getFactionManager().getFactionByPlayer(player.getUniqueId());
-        if (f == null) {
-            return;
-        }
         List<PotionEffect> effects = new ArrayList<>();
         if(intensity > 5) {
             effects.add(new PotionEffect(PotionEffectType.HUNGER, 20 * 60 * 60, intensity));
@@ -38,5 +35,33 @@ public class EffectManager {
             effects.add(new PotionEffect(PotionEffectType.WEAKNESS, 20 * 60 * 60, intensity));
         }
         player.addPotionEffects(effects);
+    }
+
+    public void update() {
+        PoliticsManager polMan = Rivals.getPoliticsManager();
+        for(Faction f : Rivals.getFactionManager().getFactions()) {
+            for(UUID playerID : f.getMembers()) {
+                Player p = Bukkit.getPlayer(playerID);
+                if(p != null) {
+                    updatePlayer(p, polMan);
+                }
+            }
+        }
+    }
+
+    public void updatePlayer(Player p, PoliticsManager polMan) {
+        long time = System.currentTimeMillis();
+        Faction f = Rivals.getFactionManager().getFactionByPlayer(p.getUniqueId());
+        int penalty = 0; //f.getWarMongering();
+        if(polMan.getDenouncedFactions().getOrDefault(f.getID(), 0L) > time) {
+            penalty += 1;
+        }
+        if(polMan.getSanctionedFactions().getOrDefault(f.getID(), 0L) > time) {
+            penalty += 2;
+        }
+        if(polMan.getInterventionFactions().getOrDefault(f.getID(), 0L) > time) {
+            penalty += 3;
+        }
+        applyEffects(p, penalty);
     }
 }
